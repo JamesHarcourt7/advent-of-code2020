77512d223bdc4b463938f5bb3a1b53608b24b3ef
James Harcourt <jh3g20@soton.ac.uk>
2020-12-13 12:56:09 +0000

    Updated Day5.java
    Added the solution for part 2 of day 5.

--- day5/Day5.java
+++ day5/Day5.java
@@ -6,10 +6,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
  class Day5 {
 
      public static int partOne(ArrayList<String> lines) {
+         // Keep track of the highest seat so far.
          int highestSeat = 0;
          for (String code : lines) {
              int seat = getSeat(code);
@@ -18,7 +20,30 @@ import java.util.ArrayList;
          return highestSeat;
      }
 
+     public static int partTwo(ArrayList<String> lines) {
+         ArrayList<Integer> seats = new ArrayList<Integer>();
+         for (String code : lines) {
+            seats.add(getSeat(code));
+         }
+         // Put the seats into ascending order.
+         // Our seat is that which is missing from the range seats[0] - seats[-1]
+         Collections.sort(seats);
+         int count = 0;
+         for (int i = 0; i < seats.size(); i ++) {
+             // Keep a counter to compare to the seat number.
+             // If a number is missed from the order, that must be our seat.
+             if (count == 0) count = seats.get(i);
+             if (count != seats.get(i)) return count;
+             count ++;
+         }
+         return 0;
+     }
+
      public static int getSeat(String code) {
+         // Looking at the seat codes, the first 7 characters are the row number in unsigned binary
+         // while the last 3 characters are the column number in unsigned binary.
+         // Finding the seat id is a matter of converting the strings to unsigned integers and doing
+         // seat = (row * 8) + column.
          String bincode = "";
          for (int i = 0; i < 7; i ++) {
              bincode += code.substring(i, i + 1).equals("B") ? "1" : "0";
@@ -33,9 +58,11 @@ import java.util.ArrayList;
      }
 
      public static void main(String[] args) {
+         // Create array list to store the contents of the input file for easy access and reuse.
          ArrayList<String> lines = new ArrayList<String>();
          String line;
          try {
+             // Create buffered reader to parse input file.
              BufferedReader br = new BufferedReader(new FileReader("day5.txt"));
              while ((line = br.readLine()) != null) {
                  lines.add(line);
@@ -43,6 +70,7 @@ import java.util.ArrayList;
              br.close();
 
              System.out.println("Part one results: " + partOne(lines));
+             System.out.println("Part two results: " + partTwo(lines));
 
          } catch (IOException e) {
              e.printStackTrace();
